c3b375b5ab672ecfc9950f9d34c4ca34f47ac8e1
James Min <jmin499@gmail.com>
2019-10-29 18:54:21 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,12 @@
 # Make your shoe class here!
 class Shoe
   
-  attr_reader :title
-  attr_accessor :brand
+  attr_reader :title, :brand
+  # attr_accessor :brand
   
   def initialize(title)
     @title = title
+    @brand = brand
   end
   
 end
