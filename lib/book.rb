99d9a81de92b6ff3e3ebdc5602ee4183f8a61436
James Min <jmin499@gmail.com>
2019-10-29 18:29:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,12 @@
 class Book
-  attr_accessor :title
+  attr_accessor :title, :author
   
   def initialize(title)
     @title = title
   end
 end
+
+
 
 book = Book.new("And Then There Were None")
 p book.title
